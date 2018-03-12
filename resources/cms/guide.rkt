ea86c7be129408ecc8750341b93c36c3f8b1d44f
Shark <xfcjscn@gmail.com>
2018-03-12 08:34:18 +0800

    一个意被另一个意打断了再找回来很花精力, group

--- resources/cms/guide.rkt
+++ resources/cms/guide.rkt
@@ -102,9 +102,104 @@ empty
 
 (first '(1 2))
 
+(byte? 1)
+
+(byte? 256)
+
+#"Apple"
+
+(bytes-ref #"Apple" 0)
+
+(make-bytes 3 65)
 
 
+(bytes->string/utf-8 #"\316\273")
+(bytes->string/latin-1 #"\316\273")
 
 
+(string->symbol "apple")
+(string->keyword "apple")
+
+'#:apple
+
+(pair? null)
+(list? null)
+(pair? empty)
+(list? empty)
+
+
+(eq? empty null)
+
+
+(write "d")
+(display "d")
+
+(for-each display '(1 2 3))
+(map display '(1 2 3))
+
+
+(member "b" '("a" "b" "c"))
+
+(assoc 'where '((when "3:00") (who "Shark") (where "SH")))
+
+(define p (mcons 1 2))
+
+p
+
+(pair? p)
+
+(mpair? p)
+
+(car p)
+
+(mcar p)
+
+; vector
+#(a b)
+#("a" "b")
+
+(vector-ref #("a" "b" "c") 2)
+
+
+(vector->list #("d" "c"))
+
+
+; hash table
+
+(define ht (make-hash))
+(hash-set! ht 'apple 'red)
+(hash-ref ht 'apple)
+
+(define ht2 (hash 'apple 'red 'banana 'yello))
+
+(hash-ref ht2 'apple)
+
+(define ht3 (hash-set ht2 'coconut 'brown))
+
+(hash-ref ht2 'coconut)
+(hash-ref ht3 'coconut)
+
+#hash(("a" . "b") ("c" . "d"))
+
+(define ht4 (make-weak-hash))
+
+ht4
+
+
+;box
+(define b (box "a"))
+
+b
+
+(unbox b)
+
+(set-box! b "c")
+
+b
+
+; void
+(void 1 2)
+
+; #<undefined> result of a reference(variable?) whose value not yet available
 
 
